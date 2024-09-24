fcc6b35b02f8c96029a95a85e82143f90a2d7c5c
Angel Leon <angeleonh2000@gmail.com>
2024-09-24 13:05:01 -0500

    Redirecci[on de /Categorias a /Tienda

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NavBar from './componentes/NavBar/NavBar'
 import ItemListContainer from './componentes/Body/ItemListContainer/ItemListContainer'
 import ProductPage from './componentes/Body/Pages/ProductPage/ProductPage'
 import useCarrito from './componentes/Hooks/useCarrito'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import useCategorias from './componentes/Hooks/useCategorias'
 
 function App() {
@@ -25,7 +25,7 @@ function App() {
       <Routes >
         <Route index element={<ItemListContainer gretting="Inicio" carritoHook={carritoHook} categoria={false} />} />
         <Route path="/Tienda" element={<ItemListContainer gretting="Bienvenido a la tienda" carritoHook={carritoHook} categoria={true} />} />
-        <Route path="/Tienda/Categoria" element={Route} />
+        <Route path="/Tienda/Categoria" element={<Navigate to="/Tienda" />} />
         <Route path='/Tienda/Categoria/:NombreCategoria' element={<ItemListContainer gretting="Categoria tipo" carritoHook={carritoHook} categoria={false} />} />
         <Route path='/Tienda/Producto/:SKU' element={<ProductPage/>} />
         <Route path='/Nosotros' element={<ProductPage />} />
